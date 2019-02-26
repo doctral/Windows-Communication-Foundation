ff632a2f37f74f4b119c93eb2ce3c833aa4f2de8
doctral <lifei.8886196@gmail.com>
2019-02-26 14:31:56 -0600

    add self-hosting using console app

---
+++ WCF.Services/WCF.SelfHostingApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCF.Services;
+
+namespace WCF.SelfHostingApp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			try {
+				ServiceHost serviceHost = new ServiceHost(typeof(Service));
+				serviceHost.Open();
+				Console.WriteLine("Press any key to abort hosting... ...");
+				Console.ReadKey();
+				serviceHost.Close();
+			}
+			catch (Exception ex) {
+				Console.WriteLine(ex.Message);
+				Console.ReadKey();
+			}
+			
+		}
+	}
+}
